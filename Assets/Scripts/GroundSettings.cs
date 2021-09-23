87eb0f07ab1463f9516f8049db0212064c36d1d8
Jonah <jonahcheyette@gmail.com>
2021-09-22 21:13:41 -0700

    adding in a few more assets, making it a bit more User Friendly

--- Assets/Scripts/GroundSettings.cs
+++ Assets/Scripts/GroundSettings.cs
@@ -8,12 +8,28 @@ public class GroundSettings : ScriptableObject {
 
     public Vector3 albedo = Vector3.one * 0.5f;
     public Vector3 specular = Vector3.one * 0.05f;
+    [Range(0, 1)]
     public float smoothness = 0.2f;
     public Vector3 emission = Vector3.zero;
 
     private void OnValidate() {
+        ClampValues();
         if (OnValuesUpdated != null) {
             OnValuesUpdated();
         }
+    }
+
+    private void ClampValues() {
+        albedo.x = Mathf.Clamp01(albedo.x);
+        albedo.y = Mathf.Clamp01(albedo.y);
+        albedo.z = Mathf.Clamp01(albedo.z);
+
+        specular.x = Mathf.Clamp01(specular.x);
+        specular.y = Mathf.Clamp01(specular.y);
+        specular.z = Mathf.Clamp01(specular.z);
+
+        emission.x = Mathf.Clamp01(emission.x);
+        emission.y = Mathf.Clamp01(emission.y);
+        emission.z = Mathf.Clamp01(emission.z);
     }
 }
